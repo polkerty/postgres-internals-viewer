c7d874abc995cd794c632d5babe70e205d4da240
Jacob Brazeal <jacob.brazeal@gmail.com>
2024-12-23 19:47:31 -0800

    parse header correctly

--- api/heap-table.js
+++ api/heap-table.js
@@ -3,6 +3,13 @@ import { open } from 'fs/promises'
 const PAGE_SIZE = 8192;
 const HEADER_SIZE = 24;
 
+function bufToBytes(buf) {
+    let ret = '';
+    for (const byte of buf) {
+        ret += byte.toString(16) + ' '
+    }
+    return ret;
+}
 
 class Group {
     constructor(name, position) {
@@ -14,58 +21,50 @@ class Group {
 
     add(slot) {
         this.slots.push(slot);
+        slot.position = this.length;
         this.length += slot.length;
+    }
+
+    toString() {
+        let out = 'Group: ' + this.name + '\n';
+        for ( const slot of this.slots) {
+            out += '\t' + slot.toString() + '\n';
+        }
+        return out;
     }
 }
 
 class Slot {
-    constructor(name, buf, position) {
+    constructor(name, buf) {
         this.name = name;
         this.buf = buf;
-        this.position = position;
         this.length = buf.length;
+    }
+
+    toString() {
+        return 'Slot: ' + this.name + ' | Length: ' + this.length + ' | Contents: ' + bufToBytes(this.buf)
     }
 }
 
 function makeHeapTablePageHeader(buf) {
-    const group = new Group("Header", [], 0, 24);
-    group.add(new Slot("pd_lsn", Buffer.from(buf.buffer, 0, 8), 0, 8));
-}
-
-class HeapTablePageHeader {
-    constructor(buf) {
-        this
-
-        console.log("Header buff: ", buf);
-        // 1. PageXLogRecPtr       pd_lsn     ( int 32, int 32 )
-        this.pd_lsn = {
-            xlogid: buf.readInt32LE(0),
-            xrecoff: buf.readInt32LE(4)
-        };
-        this.pd_checksum = buf.readInt16LE(8)
-        this.pd_lower = buf.readInt16LE(10)
-        this.pd_upper = buf.readInt16LE(12)
-        this.pd_special = buf.readInt16LE(14)
-    }
-
-    toString() {
-        return JSON.stringify({
-            pd_lsn: {
-                xlogid: this.pd_lsn.xlogid.toString(16),
-                xrecoff: this.pd_lsn.xrecoff.toString(16)
-            },
-            pd_checksum: this.pd_checksum.toString(16),
-            pd_lower: this.pd_lower.toString(16),
-            pd_upper: this.pd_upper.toString(16),
-            pd_special: this.pd_special.toString(16),
-        }, null, 2);
-    }
+    const group = new Group("Header", 0);
+    group.add(new Slot("pd_lsn", Buffer.from(buf.buffer, 0, 8)));
+    group.add(new Slot("pd_checksum", Buffer.from(buf.buffer, 8, 2)));
+    group.add(new Slot("pd_flags", Buffer.from(buf.buffer, 10, 2)));
+    group.add(new Slot("pd_lower", Buffer.from(buf.buffer, 12, 2)));
+    group.add(new Slot("pd_upper", Buffer.from(buf.buffer, 14, 2)));
+    group.add(new Slot("pd_special", Buffer.from(buf.buffer, 16, 2)));
+    group.add(new Slot("pd_prune_xid", Buffer.from(buf.buffer, 18, 2)));
+    group.add(new Slot("pd_pagesize_version", Buffer.from(buf.buffer, 20, 4)));
+    return group;
 }
 
 class HeapTablePage {
     constructor(buffer) {
         // 1. Read header
-        this.header = new HeapTablePageHeader(Buffer.from(buffer.buffer, 0, 24));
+        this.header = makeHeapTablePageHeader(Buffer.from(buffer.buffer, 0, 24));
+
+        this.groups = [this.header];
 
 
 
@@ -78,6 +77,8 @@ class HeapTablePage {
 function parseHeapTablePage(buf) {
     console.log("Page: ", buf);
     const page = new HeapTablePage(buf);
+
+    console.log(page);
 
     console.log("Header: ", page.header.toString())
 
